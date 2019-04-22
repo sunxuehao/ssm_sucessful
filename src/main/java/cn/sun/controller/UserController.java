2aa7590954343fe9d9012303faa37662b16716d0
SXH <836202892@qq.com>
2019-04-22 20:51:55 +0800

    4 22更新

--- src/main/java/cn/sun/controller/UserController.java
+++ src/main/java/cn/sun/controller/UserController.java
@@ -2,6 +2,7 @@ package cn.sun.controller;
 
 import cn.sun.pojo.User;
 import cn.sun.service.UserService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,7 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <body>
 <h2>Hello World!</h2>
+<a href="/user/login">点击登录</a>
 </body>
 </html>
